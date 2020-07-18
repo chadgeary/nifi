b4c5e6fbc42ece071cc1d3614af29eb07d8728b2
chadgeary <chad.geary@gmail.com>
2020-07-18 03:24:13 +0000

    nat gateways

--- tf-nifi.tf
+++ tf-nifi.tf
@@ -78,29 +78,46 @@ resource "aws_route_table" "tf-nifi-pubrt" {
   }
 }
 
-resource "aws_route_table" "tf-nifi-prirt" {
+resource "aws_route_table" "tf-nifi-prirt1" {
   vpc_id                  = aws_vpc.tf-nifi-vpc.id
   tags                    = {
-    Name                  = "tf-nifi-prirt"
+    Name                  = "tf-nifi-prirt1"
+  }
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.tf-nifi-ng1.id
   }
 }
 
-# subnets
+resource "aws_route_table" "tf-nifi-prirt2" {
+  vpc_id                  = aws_vpc.tf-nifi-vpc.id
+  tags                    = {
+    Name                  = "tf-nifi-prirt2"
+  }
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.tf-nifi-ng2.id
+  }
+}
+
+resource "aws_route_table" "tf-nifi-prirt3" {
+  vpc_id                  = aws_vpc.tf-nifi-vpc.id
+  tags                    = {
+    Name                  = "tf-nifi-prirt3"
+  }
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.tf-nifi-ng3.id
+  }
+}
+
+# public subnets
 resource "aws_subnet" "tf-nifi-pubnet1" {
   vpc_id                  = aws_vpc.tf-nifi-vpc.id
   availability_zone       = data.aws_availability_zones.tf-nifi-azs.names[0]
   cidr_block              = var.pubnet1_cidr
   tags                    = {
     Name                  = "tf-nifi-pubnet1"
-  }
-}
-
-resource "aws_subnet" "tf-nifi-prinet1" {
-  vpc_id                  = aws_vpc.tf-nifi-vpc.id
-  availability_zone       = data.aws_availability_zones.tf-nifi-azs.names[0]
-  cidr_block              = var.prinet1_cidr
-  tags                    = {
-    Name                  = "tf-nifi-prinet1"
   }
 }
 
@@ -113,15 +130,6 @@ resource "aws_subnet" "tf-nifi-pubnet2" {
   }
 }
 
-resource "aws_subnet" "tf-nifi-prinet2" {
-  vpc_id                  = aws_vpc.tf-nifi-vpc.id
-  availability_zone       = data.aws_availability_zones.tf-nifi-azs.names[1]
-  cidr_block              = var.prinet2_cidr
-  tags                    = {
-    Name                  = "tf-nifi-prinet2"
-  }
-}
-
 resource "aws_subnet" "tf-nifi-pubnet3" {
   vpc_id                  = aws_vpc.tf-nifi-vpc.id
   availability_zone       = data.aws_availability_zones.tf-nifi-azs.names[2]
@@ -131,16 +139,7 @@ resource "aws_subnet" "tf-nifi-pubnet3" {
   }
 }
 
-resource "aws_subnet" "tf-nifi-prinet3" {
-  vpc_id                  = aws_vpc.tf-nifi-vpc.id
-  availability_zone       = data.aws_availability_zones.tf-nifi-azs.names[2]
-  cidr_block              = var.prinet3_cidr
-  tags                    = {
-    Name                  = "tf-nifi-prinet3"
-  }
-}
-
-# route table associations
+# public route table associations
 resource "aws_route_table_association" "rt-assoc-pubnet1" {
   subnet_id               = aws_subnet.tf-nifi-pubnet1.id
   route_table_id          = aws_route_table.tf-nifi-pubrt.id
@@ -156,17 +155,95 @@ resource "aws_route_table_association" "rt-assoc-pubnet3" {
   route_table_id          = aws_route_table.tf-nifi-pubrt.id
 }
 
+# nat gateways per subnet
+resource "aws_eip" "tf-nifi-ng-eip1" {
+  vpc                     = true
+  tags                    = {
+    Name                  = "tf-nifi-ng-eip1"
+  }
+}
+
+resource "aws_eip" "tf-nifi-ng-eip2" {
+  vpc                     = true
+  tags                    = {
+    Name                  = "tf-nifi-ng-eip2"
+  }
+}
+
+resource "aws_eip" "tf-nifi-ng-eip3" {
+  vpc                     = true
+  tags                    = {
+    Name                  = "tf-nifi-ng-eip3"
+  }
+}
+
+resource "aws_nat_gateway" "tf-nifi-ng1" {
+  allocation_id           = aws_eip.tf-nifi-ng-eip1.id
+  subnet_id               = aws_subnet.tf-nifi-pubnet1.id
+  tags                    = {
+    Name                  = "tf-nifi-ng1"
+  }
+  depends_on              = [aws_internet_gateway.tf-nifi-gw]
+}
+
+resource "aws_nat_gateway" "tf-nifi-ng2" {
+  allocation_id           = aws_eip.tf-nifi-ng-eip2.id
+  subnet_id               = aws_subnet.tf-nifi-pubnet2.id
+  tags                    = {
+    Name                  = "tf-nifi-ng2"
+  }
+  depends_on              = [aws_internet_gateway.tf-nifi-gw]
+}
+
+resource "aws_nat_gateway" "tf-nifi-ng3" {
+  allocation_id           = aws_eip.tf-nifi-ng-eip3.id
+  subnet_id               = aws_subnet.tf-nifi-pubnet3.id
+  tags                    = {
+    Name                  = "tf-nifi-ng3"
+  }
+  depends_on              = [aws_internet_gateway.tf-nifi-gw]
+}
+
+# private subnets
+resource "aws_subnet" "tf-nifi-prinet1" {
+  vpc_id                  = aws_vpc.tf-nifi-vpc.id
+  availability_zone       = data.aws_availability_zones.tf-nifi-azs.names[0]
+  cidr_block              = var.prinet1_cidr
+  tags                    = {
+    Name                  = "tf-nifi-prinet1"
+  }
+}
+
+resource "aws_subnet" "tf-nifi-prinet2" {
+  vpc_id                  = aws_vpc.tf-nifi-vpc.id
+  availability_zone       = data.aws_availability_zones.tf-nifi-azs.names[1]
+  cidr_block              = var.prinet2_cidr
+  tags                    = {
+    Name                  = "tf-nifi-prinet2"
+  }
+}
+
+resource "aws_subnet" "tf-nifi-prinet3" {
+  vpc_id                  = aws_vpc.tf-nifi-vpc.id
+  availability_zone       = data.aws_availability_zones.tf-nifi-azs.names[2]
+  cidr_block              = var.prinet3_cidr
+  tags                    = {
+    Name                  = "tf-nifi-prinet3"
+  }
+}
+
+# private route table associations
 resource "aws_route_table_association" "rt-assoc-prinet1" {
   subnet_id               = aws_subnet.tf-nifi-prinet1.id
-  route_table_id          = aws_route_table.tf-nifi-prirt.id
+  route_table_id          = aws_route_table.tf-nifi-prirt1.id
 }
 
 resource "aws_route_table_association" "rt-assoc-prinet2" {
   subnet_id               = aws_subnet.tf-nifi-prinet2.id
-  route_table_id          = aws_route_table.tf-nifi-prirt.id
+  route_table_id          = aws_route_table.tf-nifi-prirt2.id
 }
 
 resource "aws_route_table_association" "rt-assoc-prinet3" {
   subnet_id               = aws_subnet.tf-nifi-prinet3.id
-  route_table_id          = aws_route_table.tf-nifi-prirt.id
+  route_table_id          = aws_route_table.tf-nifi-prirt3.id
 }
